61de583f945eaf77df053fa8414aecaaf85d16ed
Bolaolat <bolaolat300@gmail.com>
2024-10-07 00:23:05 +0100

    Update config.js

--- config.js
+++ config.js
@@ -76,8 +76,8 @@ module.exports = {
  
   author : process.env.PACK_AUTHER|| "",
   packname: process.env.PACK_NAME || "",
-  botname : process.env.BOT_NAME  || "Suhail-XMD",
-  ownername:process.env.OWNER_NAME|| "Suhail-X",
+  botname : process.env.BOT_NAME  || "BLUE DEMON",
+  ownername:process.env.OWNER_NAME|| "BLUE DEMON",
 
 
   errorChat : process.env.ERROR_CHAT || "",
